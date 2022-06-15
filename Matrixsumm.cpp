13723326a8ec0a30b7d1c0d7911b6c7b62e06803
alekseypavlov4 <97864682+alekseypavlov4@users.noreply.github.com>
2022-06-15 19:34:56 +1000

    Add files via upload

---
+++ Matrixsumm.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    srand(time(NULL));
+    const int N = 2;
+    int mtrx[N] = {-1, 1};
+    cout << "Razmer matrix: ";
+    cin >> n;
+    int arr[n][n];
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            arr[i][j] = mtrx[rand()%2];
+            cout << setw(3) << arr[i][j] << " ";
+            if (j >= (n -1))
+            {
+                cout << endl;
+            }
+        }
+    }
+
+    int sum = 0;
+    
+    for (int i = 0; i < n-1; i++)
+    {
+        for (int j = 0; j < n-1; j++)
+        {
+            sum += arr[i][j]*arr[i+1][j]+arr[i][j]*arr[i][j+1];
+        }
+    }
+    
+    for (int i = 0; i < n-2; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            sum += arr[i][j]*arr[n-1][j];
+        }
+    }
+    
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n-2; j++)
+        {
+            sum += arr[i][j]*arr[i][n-1];
+        }
+    }
+    
+    for (int i = 0; i < n-1; i++)
+    {
+        int j = n-1;
+        {
+            sum += arr[i][j]*arr[i+1][j];
+        }
+    }
+    
+    for (int j = 0; j < n-1; j++)
+    {
+        int i = n-1;
+        {
+            sum += arr[i][j]*arr[i][j+1];
+        }
+    }
+    
+    sum += arr[n-1][n-1];
+    
+    cout << endl;
+    cout << sum << endl;
+    return 0;
+}
