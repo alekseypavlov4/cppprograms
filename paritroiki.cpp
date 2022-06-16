81b0bd53620602cbe213836a91c961515ad75ab4
alekseypavlov4 <97864682+alekseypavlov4@users.noreply.github.com>
2022-06-16 23:44:22 +1000

    Update paritroiki.cpp

--- paritroiki.cpp
+++ paritroiki.cpp
@@ -7,55 +7,55 @@ using namespace std;
 
 int main()
 {
-	int array[100];
+	int A[100];
 	srand(time(NULL));
 	
 	for(int i=0; i<100; i++)
 	{
-		array[i]=rand()%100;
-		cout << setw(3) << array[i] << endl;
+		A[i]=rand()%100;
+		cout << i+1 << ")" << setw(3) << A[i] << endl;
 	}
 	
-	cout<<"\t"<<endl;
-	cout<<"\t"<<endl;
+	cout<<endl;
+	cout<<endl;
 	
 	int min;
 	for (int i=0; i<100-1; i++)
 	{
 		for (int j=0; j<100-i-1; j++)
 		{
-			if (array[j]>array[j+1])
+			if (A[j]>A[j+1])
 			{
-			min=array[j];
-			array[j]=array[j+1];
-			array[j+1]=min;
+			min=A[j];
+			A[j]=A[j+1];
+			A[j+1]=min;
 			}
 		}
 	}
 	
 	for (int i=0; i<100; i++)
 	{
-		cout << setw(3) <<array[i]<<endl;
+		cout << i+1 << ")" << setw(3) <<A[i]<<endl;
 	}
 
-	cout<<"\t"<<endl;
-	cout<<"\t"<<endl;
+	cout<<endl;
+	cout<<endl;
 
 	int k=0;
 	for (int i=0; i<100-1; i++)
 	{
-		if(array[i]==array[i+1])
+		if(A[i]==A[i+1])
 		{
 			k++;
 		}
 		else
 		{
-			cout<<setw(3)<<array[i]<<" : "<<"kol-vo = "<<k+1<<","<<"pari = "<<(k+1)/2<<","<<"troiki = "<<(k+1)/3<<"\t"<<endl;
+			cout<<setw(3)<<A[i]<<" : "<<"kol-vo = "<<k+1<<","<<"par = "<<(k+1)/2<<","<<"troek = "<<(k+1)/3<<"\t"<<endl;
 			k=0;
 		}
 		if (i==100-2)
 		{
-		cout<<setw(3)<<array[i]<<" : "<<"kol-vo = "<<k+1<<","<<"pari = "<<(k+1)/2<<","<<"troiki = "<<(k+1)/3<<"\t"<<endl;
+			cout<<setw(3)<<A[i]<<" : "<<"kol-vo = "<<k+1<<","<<"par = "<<(k+1)/2<<","<<"troek = "<<(k+1)/3<<"\t"<<endl;
 		}
 	}
 
